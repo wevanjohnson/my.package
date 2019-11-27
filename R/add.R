8735c94828960531a67e75221222e85d51cbf456
W. Evan Johnson <wej@bu.edu>
2019-11-27 10:44:35 -0500

    this is the first commit of my R package

---
+++ R/add.R
@@ -0,0 +1,17 @@
+#' This is my addition function
+#'
+#' @param x this is the first value to add
+#' @param y this is the second value to add
+#'
+#' @return This function returns the sum of x and y
+#'
+#' @examples
+#' ## Start with something simple
+#' add(1,1)
+#'
+#' ## Now something more difficult
+#' add(49,60)
+#'
+#' @export
+
+add <- function(x,y){x+y}
---
+++ R/subtract.R
@@ -0,0 +1,16 @@
+#' This is my subtract function
+#'
+#' @param x this is the first value
+#' @param y this is the second value to subtract
+#'
+#' @return This function returns the difference of x and y
+#'
+#' @examples
+#' ## Start with something simple
+#' subtract(1,1)
+#'
+#' ## Now something more difficult
+#' subtract(49,60)
+#'
+#' @export
+subtract <- function(x,y){x-y}
