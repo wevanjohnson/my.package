6a8648aa4246f1e44f9c02f3126b1b404438c978
nisreenkhambati <nkhambati1990@gmail.com>
2023-04-24 08:23:07 +0100

    this added a multiplication file and modified my description again

---
+++ R/multiply.R
@@ -0,0 +1,18 @@
+#' This is my multiplication function
+#'
+#' @param x this is the first value to multiply
+#' @param y this is the second value to multiple
+#'
+#' @return This function returns the product of x and y
+#'
+#' @examples
+#' ## Start with something simple
+#' product(1,1)
+#'
+#' ## Now something more difficult
+#' product(49,60)
+#'
+#' @export
+
+
+add <- function(x,y){x*y}
