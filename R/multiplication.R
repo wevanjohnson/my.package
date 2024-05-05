1bf3d48e7f2de0efe516942fdf19235ad580992c
Azuama Onyedikachi <azuamaonyedikachi@Azuamas-MacBook-Pro.local>
2024-05-05 17:04:39 -0400

    I modified the DESCRIPTION to include Onyedikachi Azuama as a co-author, and added a multiplication function to the R folder

---
+++ R/multiplication.R
@@ -0,0 +1,17 @@
+#' This is my multiplication function
+#'
+#' @param x this is the first value to multiply
+#' @param y this is the second value to multiply
+#'s
+#' @return This function returns the multiplication of x and y
+#'
+#' @exampless
+#' ## Start with something simple
+#' multiply(1,1)
+#'
+#' ## Now something more difficult
+#' multiply(49,60)
+#'
+#' @export
+
+multiply <- function(x,y){x*y}
