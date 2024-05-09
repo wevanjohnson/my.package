ea04bf13687d041d1d64136e0988733ed1cd687f
Ankita Prakash <ankitaprakash@macbook-pro.myfiosgateway.com>
2024-05-08 21:22:47 -0400

    Add multiply function
    * ap2006 submission for HW1

---
+++ R/multiply.R
@@ -0,0 +1,17 @@
+#' This is my multiplication function
+#'
+#' @param x this is the first value to multiply
+#' @param y this is the second value to multiply
+#'
+#' @return This function returns the product of x and y
+#'
+#' @examples
+#' ## Start with something simple
+#' add(1,1)
+#'
+#' ## Now something more difficult
+#' add(49,60)
+#'
+#' @export
+
+multiply <- function(x,y){x*y}
