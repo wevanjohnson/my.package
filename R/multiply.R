f0a5b5aab4970da65bfe092992d2698d83a99122
Angelica Lee <angelicaejlee@gmail.com>
2025-02-06 15:44:23 -0500

    Added multiply function

---
+++ R/multiply.R
@@ -0,0 +1,14 @@
+#' Multiply two numbers
+#'
+#' This function multiplies two numeric values.
+#'
+#' @param a A numeric value.
+#' @param b A numeric value.
+#' @return The product of a and b.
+#' @examples
+#' multiply(2, 3)
+#' multiply(4, 5)
+multiply <- function(a, b) {
+  return(a * b)
+}
+
