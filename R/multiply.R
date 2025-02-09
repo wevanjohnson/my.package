a50289f378073a85274088678a2c738ed1f04877
Rubin K <rkaur1290@hotmail.com>
2025-02-08 19:03:22 -0500

    added multiplication

--- R/multiply.R
+++ R/multiply.R
@@ -1,3 +1,17 @@
-multiply <- function(a, b) {
-	return(a * b)
-}
+#' This is my multiplication function
+#'
+#' @param x this is the first value to multiply
+#' @param y this is the second value to multiply
+#'
+#' @return This function returns the product of x and y
+#'
+#' @examples
+#' ## Start with something simple
+#' product(1,1)
+#'
+#' ## Now something more difficult
+#' product(49,60)
+#'
+#' @export
+
+product <- function(x,y){x*y}
