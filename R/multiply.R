cb5b57305917be4de19fbb1f95d888621170b121
yujinkim22 <wilstarlow@gmail.com>
2025-02-10 16:18:54 -0500

    Added Yujin Kim as an author and implemented a multiplication function

---
+++ R/multiply.R
@@ -0,0 +1,13 @@
+#' Multiply Two Numbers
+#'
+#' This function multiplies two numeric inputs.
+#'
+#' @param x A numeric value.
+#' @param y A numeric value.
+#' @return The product of \code{x} and \code{y}.
+#' @export
+#' @examples
+#' multiply(2, 3) # returns 6
+multiply <- function(x, y) {
+  return(x * y)
+}
