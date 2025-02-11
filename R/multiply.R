6d3719cf5d8aea7e63745be4a3994dcab07d526e
Joseph Tantillo <Josephtantillo6@Josephs-Air.localdomain>
2025-02-10 20:23:28 -0500

    Added multiply function and updated DESCRIPTION file with my name as author.

---
+++ R/multiply.R
@@ -0,0 +1,13 @@
+#' Multiply two numbers
+#'
+#' This function multiplies two numeric values and returns the result.
+#'
+#' @param a A numeric value to be multiplied.
+#' @param b A numeric value to be multiplied.
+#' @return The product of `a` and `b`.
+#' @examples
+#' multiply(2, 3)  # Returns 6
+#' multiply(5, 10) # Returns 50
+multiply <- function(a, b) {
+  return(a * b)
+}
