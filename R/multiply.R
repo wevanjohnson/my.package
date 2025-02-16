b21bd2f0d5e5c1a6d3d99585fdaeb99e14f29673
Danyal Siddiqui <danyal1s@hotmail.com>
2025-02-16 18:06:26 -0500

    updated description and added multiplication -DS

---
+++ R/multiply.R
@@ -0,0 +1,17 @@
+#' This is Danyal's multiplication function
+#'
+#' @param x this is the first value to multiply
+#' @param y this is the second value to multiply
+#'
+#' @return This function returns the product of x and y
+#'
+#' @examples
+#' ## Start with something simple
+#' add(2,3)
+#'
+#' ## Now something more difficult
+#' add(345,678)
+#'
+#' @export
+
+multiply <- function(x,y){x*y}
